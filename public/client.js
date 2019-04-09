af05760f89d3e91bcd94ac8519340ccfb2f4743e
Glitch (gelatinous-elk) <none>
2019-04-09 08:56:00 +0000

    🎥🎟 Checkpoint ./public/client.js:3759713/866

--- public/client.js
+++ public/client.js
@@ -43,8 +43,8 @@ const createTextCell = (text) => {
 
 const getList = () => {
   axios.get('https://api.airtable.com/v0/appf7mrRY6a3xK8jT/Subscriptions?maxRecords=3&view=Grid%20view', { 
-                                headers: { Authorization: "Bearer "+'key1t0VRiFHd7Kuj8'} 
-                            }).then(function (res) {
+        headers: { Authorization: "Bearer "+'key1t0VRiFHd7Kuj8'} 
+    }).then(function (res) {
     console.log(res.data.records)
     let container = document.getElementById('result_table')
     container.innerHTML = ""
